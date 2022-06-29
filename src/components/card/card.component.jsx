0f67eb84829043ca6c1cd09abe568e2722d452ad
mohammed-najib <mohammed.njeeb@gmail.com>
2022-06-29 17:46:43 +0300

    init

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,31 @@
+// import { Component } from "react";
+
+import './card.styles.css';
+
+const Card = ({monster}) => {
+    const {id, name, email} = monster;
+
+    return (
+        <div key={id} className="card-container">
+            <img src={`https://robohash.org/${id}?set=set2&size=180x180`} alt={`monster ${name}`} />
+            <h2>{name}</h2>
+            <p>{email}</p>
+        </div>
+    )
+}
+
+// class Card extends Component {
+//     render() {
+//         const {id, name, email} = this.props.monster;
+
+//         return (
+//             <div key={id} className="card-container">
+//                 <img src={`https://robohash.org/${id}?set=set2&size=180x180`} alt={`monster ${name}`} />
+//                 <h2>{name}</h2>
+//                 <p>{email}</p>
+//             </div>
+//         );
+//     }
+// }
+
+export default Card;
